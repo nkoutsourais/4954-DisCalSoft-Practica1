9b001d1d737953bc69734b0877bd838663151a0f
Neofitos Koutsourais <nkoutsourais@gmail.com>
2019-09-29 10:21:42 +0200

    Versión 1.0

--- src/main/java/com/practica1/Combination.java
+++ src/main/java/com/practica1/Combination.java
@@ -2,17 +2,17 @@ package com.practica1;
 
 import java.util.stream.Stream;
 
-public class Combination {
+public abstract class Combination {
     private static final int MAX_COLORS = 4;
 
     private Color[] colors = new Color[MAX_COLORS];
 
-    public void setColor(Color color, int position) {
+    protected void setColor(Color color, int position) {
         positionControl(position);
         this.colors[position] = color;
     }
 
-    public Color getColor(int position) {
+    protected Color getColor(int position) {
         positionControl(position);
         return this.colors[position];
     }
@@ -22,7 +22,7 @@ public class Combination {
             throw new IllegalArgumentException("The position is wrong");
     }
 
-    public boolean isComplete() {
+    protected boolean isComplete() {
         for (Color color : colors) {
             if (color == null)
                 return false;
@@ -30,21 +30,21 @@ public class Combination {
         return true;
     }
 
-    public boolean ContainsColor(Color color) {
+    protected boolean ContainsColor(Color color) {
         return Stream.of(this.colors).anyMatch(color::equals);
     }
 
-    public Color[] getColors() {
+    protected Color[] getColors() {
         return colors;
     }
 
-    public void setColors(Color[] colors) {
+    protected void setColors(Color[] colors) {
         if (colors == null || colors.length != MAX_COLORS)
             throw new IllegalArgumentException("Wrong proposed combination length");
         this.colors = colors;
     }
 
-    public void writeColors() {
+    protected void writeColors() {
         Stream.of(colors).forEach(c -> System.out.print(c.getSymbol()));
     }
 }
--- src/main/java/com/practica1/Mastermind.java
+++ src/main/java/com/practica1/Mastermind.java
@@ -16,6 +16,7 @@ public class Mastermind {
         do {
             System.out.println("----- MASTERMIND -----");
             secretPlayer.prepare();
+            secretPlayer.write();
             do {
                 proposedPlayer.propose(scanner);
                 proposedPlayer.writeAttempts();
